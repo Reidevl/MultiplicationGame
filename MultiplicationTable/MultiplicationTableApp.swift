4b550ffd57740b7fc2d120dd02161079a474c0a4
Reiwil Lugo <reiwillugo@gmail.com>
2024-03-05 16:13:01 -0400

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Reiwil Lugo on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
